f7df7c8ff5cccc6c931bfb387fd6828557d434d9
aamacaya <aamacaya@uc.cl>
2020-04-18 18:12:11 -0400

    mino modifications

--- lib/latent_chord.py
+++ lib/latent_chord.py
@@ -1,14 +1,20 @@
 import numpy as np
 import tensorflow as tf
+import scipy.io.wavfile as wav
+
+def import_audio(filename):
+    audio = np.array([wav.read(filename)[1]],dtype=float)
+    return audio
 
 class latent_chord:
     
-    triads   = [None,'C','Dm','Em','F','G','Am','Bdim']
-    colors   = ['k', 'b','g' ,'r' ,'c','m','y' ,'0.5']
-    octaves  = [None, '2','3','4']
-    mkr_type = ['o', '.','*','1']
-    volumes  = [None, 'f'  ,'m'  ,'p']
-    mkr_size = [2, 500,100,20]
+    triads     = [None,'C','Dm','Em','F','G','Am','Bdim']
+    colors     = ['k', 'b','g' ,'r' ,'c','m','y' ,'0.5']
+    octaves    = [None, '2','3','4']
+    mkr_type   = ['o', '.','*','1']
+    volumes    = [None, 'f'  ,'m'  ,'p']
+    mkr_size   = [2, 500,100,20]
+    instrument = ['piano','guitar']
     
     def __init__(self, latent, model, spec_helper):
         self.latent = latent
@@ -18,6 +24,7 @@ class latent_chord:
         self.octave = None
         self.volume = None
         self.example = None
+        self.instrument = None
         
     @property
     def chord(self):
@@ -25,7 +32,7 @@ class latent_chord:
     
     @property
     def full_name(self):
-        return self.triad+self.octave+'_'+self.volume+'_'+self.example
+        return self.instrument+'_'+self.triad+self.octave+'_'+self.volume+'_'+self.example
     
     @property
     def plt_color(self):
@@ -56,16 +63,18 @@ class latent_chord:
         return audio[0,:,0]
     
     @classmethod
-    def from_file(cls, path, filename):
+    def from_file(cls, path, filename, model, spec_helper):
+        cls.model = model
+        cls.spec_helper = spec_helper
         if filename[-4:]=='.wav':
-            audio_origin = import_audio2(path+filename)[0,:]
+            audio_origin = import_audio(path+filename)[0,:]
             filename = filename[0:-4]
         else:
-            audio_origin = import_audio2(path+filename+'.wav')[0,:]
-        mel = self.spec_helper.waves_to_melspecgrams(audio_origin.reshape([1,64000,1]))
-        latent, _ = self.model.encode(mel)
-        new_latent_chord = latent_chord(latent)
-        chord_octave,new_latent_chord.volume,new_latent_chord.example = filename.split('_')
+            audio_origin = import_audio(path+filename+'.wav')[0,:]
+        mel = cls.spec_helper.waves_to_melspecgrams(audio_origin.reshape([1,64000,1]))
+        latent, _ = cls.model.encode(mel)
+        new_latent_chord = latent_chord(latent, model, spec_helper)
+        new_latent_chord.instrument, chord_octave, new_latent_chord.volume,new_latent_chord.example = filename.split('_')
         new_latent_chord.triad = chord_octave[0:-1]
         new_latent_chord.octave = chord_octave[-1:] 
         return new_latent_chord    
